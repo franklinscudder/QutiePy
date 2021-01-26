588b49a7efaf04f80ba2be304a0a2844750aa026
Tom Findlay <snick10000@live.co.uk>
2021-01-26 21:49:55 +0100

    Fixed 'scripts' in setup.py, moved examples back to root

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ from setuptools import find_packages, setup
 setup(
     name='QutiePy',
     py_modules=['qutiepy'],
-    version='0.1.11',
+    version='0.1.12',
     long_description='A WIP package providing a simple OOP framework for simulating quantum computing operations in python, with an emphasis on accessability and simplicity.',
     long_description_content_type="text/markdown",
     description='A simple package for learning about and exploring quantum computing.',
     author='T. E. L. Findlay',
     author_email="snick10000@live.co.uk",
     license='MIT',
-    scripts=['examples\\BellState.py'],
     classifiers= [
             "Development Status :: 2 - Pre-Alpha",
             "Intended Audience :: Education",
