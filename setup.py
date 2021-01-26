06e3948c86d5add3d77b4fe58009d12f5b679498
Tom Findlay <snick10000@live.co.uk>
2021-01-26 21:37:29 +0100

    version 0.1.11

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='QutiePy',
     py_modules=['qutiepy'],
-    version='0.1.10',
+    version='0.1.11',
     long_description='A WIP package providing a simple OOP framework for simulating quantum computing operations in python, with an emphasis on accessability and simplicity.',
     long_description_content_type="text/markdown",
     description='A simple package for learning about and exploring quantum computing.',
