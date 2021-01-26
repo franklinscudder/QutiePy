ce0b3d7048b130ddbb84ef682c13ad98cd586715
Tom Findlay <snick10000@live.co.uk>
2021-01-26 22:18:47 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,13 +26,13 @@ author = 'T. E. L. Findlay'
 # The full version, including alpha/beta/rc tags
 release = '0.1.12'
 
-
+import sphinxcontrib.napoleon
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinxcontrib.napoleon']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
