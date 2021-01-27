892e1ef444e253c42f9042d2e783b341bb6c2be7
Tom Findlay <snick10000@live.co.uk>
2021-01-27 05:25:55 +0100

    Added dependency management for pip

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 from setuptools import find_packages, setup
+import os
+
+thelibFolder = os.path.dirname(os.path.realpath(__file__))
+requirementPath = thelibFolder + '/requirements.txt'
+install_requires = [] 
+
+if os.path.isfile(requirementPath):
+    with open(requirementPath) as f:
+        install_requires = f.read().splitlines()
+
 setup(
     name='QutiePy',
     py_modules=['qutiepy'],
     version='0.1.14',
     long_description='A WIP package providing a simple OOP framework for simulating quantum computing operations in python, with an emphasis on accessability and simplicity.',
     long_description_content_type="text/markdown",
+    install_requires=install_requires,
     description='A simple package for learning about and exploring quantum computing.',
     author='T. E. L. Findlay',
     author_email="snick10000@live.co.uk",
